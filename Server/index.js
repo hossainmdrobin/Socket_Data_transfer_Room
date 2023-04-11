2bfef1aee502432bba8fbd6595e7066d494b2023
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-04-12 00:12:25 +0600

    sss

---
+++ Server/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        method:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log(`User connected ${socket.id}`)
+
+    // socket.on("send_message",(data)=>{
+    //     socket.broadcast.emit("receive_message",data)
+    // })
+
+    socket.on("join_room",(data)=>{
+        socket.join(data);
+    })
+
+    socket.on("send_message",(data)=>{
+        socket.to(data.room).emit("receive_message",data)
+    })
+})
+
+server.listen(3001,()=>{
+    console.log("SERVER IS RUNNING")
+} )
